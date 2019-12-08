33fb2a5ed0cbe6bc175f978aa2a06ea9401dfa26
Amanda Meneghin <amanda.meneghin@hadrion.com.br>
2019-12-08 18:48:19 -0300

    Adicionando Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListHeroesComponent } from './components/list-heroes/list-heroes.component';
 import { CardHeroComponent } from './components/card-hero/card-hero.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListHeroesComponent,
-    CardHeroComponent
+    CardHeroComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
